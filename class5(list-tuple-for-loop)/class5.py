921532703fb1045de736c52bfe830a51ca63f375
Basit Ali <basitali412@gmail.com>
2024-12-09 18:40:06 +0500

    Class 5 (List, tuple, For-Loop)

---
+++ class5(list-tuple-for-loop)/class5.py
@@ -0,0 +1,8 @@
+names : list[str] = ["basit ali", "Qamar shahzad", "Rana Haris","Hammad Afzal"]
+
+print( names[0])  #Printing string by giving index
+
+# Print List by using For Loop
+
+for name in names:
+    print(name)
